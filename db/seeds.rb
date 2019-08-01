395871564184f9dbad4925ad297db0ee87af9c68
SarahB0101 <sbouchir@gmail.com>
2019-08-01 11:24:04 +0200

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+require 'faker'
+
+User.destroy_all
+City.destroy_all
+Tag.destroy_all
+Gossip.destroy_all
+PrivateMessage.destroy_all
+
+10.times do
+  tag = Tag.create(title: Faker::Games::Pokemon.name)
+  city = City.create(name: Faker::Address.city, zip_code: Faker::Address.zip_code )
+  user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::TvShows::Friends.quote ,email: Faker::Internet.email, age: Faker::Number.number(digits: 2), city: City.all.sample)
+  
+end
+
+20.times do
+  gossip = Gossip.create(title: Faker::TvShows::BreakingBad.episode, content: Faker::TvShows::RuPaul.quote, user: User.all.sample)
+  private_message = PrivateMessage.create(content: Faker::TvShows::RuPaul.quote, recipient: User.all.sample, sender:User.all.sample)
+end
+
+20.times do
+	JoinTableGossipTag.create(tag: Tag.all.sample, gossip: Gossip.all.sample)
+end
+
